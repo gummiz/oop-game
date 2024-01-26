d15d1796182a2d02fadbbcfe5bef555f66185cda
Stefan.Kummerlöw <gummiz@gmx.net>
2024-01-26 10:50:59 +0100

    added player Class

--- js/main.js
+++ js/main.js
@@ -1,2 +1,30 @@
-let board = document.getElementById("board")
-board.style.width = "500px"
+// let board = document.getElementById("board")
+
+class Player {
+    constructor(){
+        this.positionX = 0
+        this.positionY = 0
+        this.hight = 30
+        this.width = 60
+
+    }
+
+    moveLeft(){
+        this.positionX = this.positionX - 10
+        console.log("moving left", this.positionX);
+        
+    }
+    
+    moveRight(){
+        this.positionX = this.positionX + 10
+        console.log("moving right", this.positionX );
+    }
+}
+
+const p = new Player()
+
+p.moveLeft()
+p.moveLeft()
+p.moveLeft()
+p.moveLeft()
+p.moveRight()
