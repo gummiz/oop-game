01d54c08cac3231994ddd8ce106be00a25d90d57
Stefan.Kummerlöw <gummiz@gmx.net>
2024-01-26 12:31:50 +0100

    added player movement left and right

--- js/main.js
+++ js/main.js
@@ -1,30 +1,65 @@
 // let board = document.getElementById("board")
 
 class Player {
-    constructor(){
-        this.positionX = 0
-        this.positionY = 0
-        this.hight = 30
-        this.width = 60
+  constructor() {
+    this.positionX = 100;
+    this.positionY = 30;
+    this.height = 20;
+    this.width = 20;
 
-    }
+    this.createPlayer();
+  }
 
-    moveLeft(){
-        this.positionX = this.positionX - 10
-        console.log("moving left", this.positionX);
-        
-    }
-    
-    moveRight(){
-        this.positionX = this.positionX + 10
-        console.log("moving right", this.positionX );
-    }
+  createPlayer() {
+    // create dom element
+    this.playerElm = document.createElement("div");
+
+    // set Player Size
+    this.playerElm.setAttribute("id", "player");
+    this.playerElm.style.height = this.height + "px";
+    this.playerElm.style.width = this.width + "px";
+
+    //append to board
+    const boardElm = document.getElementById("board");
+    boardElm.appendChild(this.playerElm);
+
+    // set position of Player
+    this.playerElm.style.position = "absolute";
+    this.playerElm.style.bottom = this.positionY + "px";
+    this.playerElm.style.left = this.positionX + "px";
+  }
+  moveLeft() {
+    this.positionX -= 10;
+    console.log("moving left", this.positionX);
+    this.playerElm.style.left = this.positionX + "px";
+  }
+
+  moveRight() {
+    this.positionX = this.positionX + 10;
+    console.log("moving right", this.positionX);
+    this.playerElm.style.left = this.positionX + "px";
+  }
 }
 
-const p = new Player()
+const player = new Player();
 
-p.moveLeft()
-p.moveLeft()
-p.moveLeft()
-p.moveLeft()
-p.moveRight()
+window.addEventListener("keypress", (direction) => {
+  // if (direction.keyCode === 37) {
+  //   console.log("move left");
+  //   player.moveLeft();
+  // }
+
+  switch (direction.keyCode) {
+    // left
+    case 37:
+      player.moveLeft();
+      break;
+
+    //right
+    case 39:
+      player.moveRight();
+      break;
+  }
+});
+
+
